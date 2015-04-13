b1181cf5ea1d698e9d03748c69e76db58316df48
YongCho <yongjec@gmail.com>
2015-04-13 16:48:08 -0400

    clean-up for first milestone submission

--- Module-Test/message-box-1.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-
-(require racket/gui)
-(message-box "test-1"
-             "This is a test message box 1.")
--- Module-Test/message-box-2.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-
-(require racket/gui)
-(message-box "test-2"
-             "This is a test message box 2.")
